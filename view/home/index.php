ad6c8364f9abbb84fc3341c9731931b5c00a9139
FirdaMrz <firdamareza0@gmail.com>
2024-12-27 15:58:09 +0700

    Initial commit

--- view/home/index.php
+++ view/home/index.php
@@ -8,59 +8,69 @@ $stok = $info->stok();
 
 
 <div class="row">
-			<div class="col-xs-12 col-md-6 col-lg-3">
-				<div class="panel panel-blue panel-widget ">
-					<div class="row no-padding">
-						<div class="col-sm-3 col-lg-5 widget-left">
-							<svg class="glyph stroked bag"><use xlink:href="#stroked-bag"></use></svg>
-						</div>
-						<div class="col-sm-9 col-lg-7 widget-right">
-							<div class="large"><?=$penjualan['jumlah']?></div>
-							<div class="text-muted">Transaksi</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-md-6 col-lg-3">
-				<div class="panel panel-orange panel-widget">
-					<div class="row no-padding">
-						<div class="col-sm-3 col-lg-5 widget-left">
-							<svg class="glyph stroked empty-message"><use xlink:href="#stroked-empty-message"></use></svg>
-						</div>
-						<div class="col-sm-9 col-lg-7 widget-right">
-							<div class="large"><?=$buku['jumlah']?></div>
-							<div class="text-muted">Judul Buku</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-md-6 col-lg-3">
-				<div class="panel panel-teal panel-widget">
-					<div class="row no-padding">
-						<div class="col-sm-3 col-lg-5 widget-left">
-							<svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg>
-						</div>
-						<div class="col-sm-9 col-lg-7 widget-right">
-							<div class="large"><?=$user['jumlah']?></div>
-							<div class="text-muted">User</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-md-6 col-lg-3">
-				<div class="panel panel-red panel-widget">
-					<div class="row no-padding">
-						<div class="col-sm-3 col-lg-5 widget-left">
-							<svg class="glyph stroked app-window-with-content"><use xlink:href="#stroked-app-window-with-content"></use></svg>
-						</div>
-						<div class="col-sm-9 col-lg-7 widget-right">
-							<div class="large"><?=$stok['jumlah']?></div>
-							<div class="text-muted">Stock Buku</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div><!--/.row-->
+    <div class="col-xs-12 col-md-6 col-lg-3">
+        <a href="index.php?page=penjualan">
+            <div class="panel panel-blue panel-widget">
+                <div class="row no-padding">
+                    <div class="col-sm-3 col-lg-5 widget-left">
+                        <svg class="glyph stroked bag"><use xlink:href="#stroked-bag"></use></svg>
+                    </div>
+                    <div class="col-sm-9 col-lg-7 widget-right">
+                        <div class="large"><?=$penjualan['jumlah']?></div>
+                        <div class="text-muted">Transaksi</div>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-xs-12 col-md-6 col-lg-3">
+        <a href="index.php?page=buku">
+            <div class="panel panel-orange panel-widget">
+                <div class="row no-padding">
+                    <div class="col-sm-3 col-lg-5 widget-left">
+                        <svg class="glyph stroked empty-message"><use xlink:href="#stroked-empty-message"></use></svg>
+                    </div>
+                    <div class="col-sm-9 col-lg-7 widget-right">
+                        <div class="large"><?=$buku['jumlah']?></div>
+                        <div class="text-muted">Judul Buku</div>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-xs-12 col-md-6 col-lg-3">
+        <a href="index.php?page=kasir">
+            <div class="panel panel-teal panel-widget">
+                <div class="row no-padding">
+                    <div class="col-sm-3 col-lg-5 widget-left">
+                        <svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg>
+                    </div>
+                    <div class="col-sm-9 col-lg-7 widget-right">
+                        <div class="large"><?=$user['jumlah']?></div>
+                        <div class="text-muted">User</div>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-xs-12 col-md-6 col-lg-3">
+        <a href="index.php?page=pemasok">
+            <div class="panel panel-red panel-widget">
+                <div class="row no-padding">
+                    <div class="col-sm-3 col-lg-5 widget-left">
+                        <svg class="glyph stroked app-window-with-content"><use xlink:href="#stroked-app-window-with-content"></use></svg>
+                    </div>
+                    <div class="col-sm-9 col-lg-7 widget-right">
+                        <div class="large"><?=$stok['jumlah']?></div>
+                        <div class="text-muted">Stock Buku</div>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+</div><!--/.row-->
+
+
 		
 		<div class="row">
 			<div class="col-lg-12">
